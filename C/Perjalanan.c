ef70b20ef310269cd5c67a8f4d9641a45c6d70ab
Fizryan <tdark9362@gmail.com>
2024-03-22 14:48:00 +0700

    New

--- C/Perjalanan.c
+++ C/Perjalanan.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int Start = (8 * 3600); // default awal jam dimulai pukul 08:00
+    int Start = (9 * 3600); // default awal jam dimulai pukul 09:00
 
     int speedAli = 5; // kecepatan Ali 5m/detik
     int speedBadu = 8; // kecepatan Badu 8m/detik
 
-    int jarakAli = 18000; // jarak Ali sudah 18000 meter di karnakan Ali sudah lebih dulu berangkat
+    int jarakAli = 18000; // jarak Ali sudah 18000 meter di karnakan Ali sudah lebih dulu berangkat 1 jam lebih dulu, jadi 5 * 3600 = 1800
     int jarakBadu = 0; // jarak Badu masih 0 dikarnakan Badu belum berangkat
 
-    int bertemu;
+    int bertemu = 0;
 
     while (jarakBadu <= jarakAli) { // ketika jarak Badu <= jarak ali, program akan selalu berulang / loop
         jarakAli += speedAli;  // jarak Ali akan di tambahkan kecepatan Ali
@@ -26,6 +26,7 @@ int main() {
     int menitBertemu = (bertemu % 3600) / 60; // konversi ke menit
     int detikBertemu = (bertemu % 60); // konversi ke detik
 
-    printf("Ali dan Badu bertemu pada pukul %02d:%02d:%02d\n", jamBertemu, menitBertemu, detikBertemu); // output hasil
+    printf("Titik Ali %02d titik Badu %02d\n", jarakAli, jarakBadu); // output titik Ali dan titik Badu
+    printf("Badu mendahului Ali pada pukul %02d:%02d:%02d\n", jamBertemu, menitBertemu, detikBertemu); // output hasil
     return 0; // program selesai
 }
