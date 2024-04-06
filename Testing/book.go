024e4a38660f0690f94a79ed60a004213326c19d
Fizryan <tdark9362@gmail.com>
2024-04-06 22:59:34 +0700

    New

---
+++ Testing/book.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	type Tanggal struct {
+		title         string
+		pages, prices int
+	}
+
+	var beli, jual, pasar Tanggal
+	fmt.Scan(&pasar.title, &pasar.pages, &pasat.prices)
+
+}
